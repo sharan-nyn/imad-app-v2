f8f2a59da61bb4c6f3b885bd58f6c363f382acae
Sharan Narayan <sharan.nyn@gmail.com>
2017-02-07 22:31:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName', function(req, res)
 {
-  var articleName = req.prarams.articleName;
+  var articleName = req.params.articleName;
   res.send(createTemplate(artciles[articleName]));
 });
 
